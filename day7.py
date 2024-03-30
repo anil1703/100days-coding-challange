ded0f1dbf94e80475a172ba931d625c87c4159e7
anil1703 <111416248+anil1703@users.noreply.github.com>
2024-03-30 22:39:53 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        cnt=[0]*(len(nums)+1)
+        ans=0
+        l=0
+        m=0
+        for num in nums:
+            cnt[num]+=1
+            if cnt[num]==1:
+                k-=1
+                if k<0:
+                    cnt[nums[m]]=0
+                    m+=1
+                    l=m
+            if k<=0:
+                while cnt[nums[m]]>1:
+                    cnt[nums[m]]-=1
+                    m+=1
+                ans+=m-l+1
+        return ans                        
+    
+"""LINK : https://leetcode.com/problems/subarrays-with-k-different-integers/solutions/4944398/python3-solution/"""
