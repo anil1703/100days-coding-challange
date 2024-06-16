e3497aefbb5ccb7e1762deb85029984f49d5d036
anil1703 <111416248+anil1703@users.noreply.github.com>
2024-06-16 16:41:52 +0530

    day 17

---
+++ day17.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        patches = 0 
+        miss = 1
+        i = 0 
+        while miss<=n:
+            if i <len(nums) and nums[i]<=miss:
+                miss += nums[i]
+                i += 1 
+            else:
+                miss += miss 
+                patches += 1 
+        return patches
+
+
+#https://leetcode.com/problems/patching-array/?envType=daily-question&envId=2024-06-16
